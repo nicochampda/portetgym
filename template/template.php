125d7eac9d361b0a4b2db13fc3c181d9de15d4f6
nicochampda <nicolaschampda@gmail.com>
2020-04-26 19:15:10 +0200

    Ajout de la barre de menu sur toutes les pages

--- template/template.php
+++ template/template.php
@@ -30,6 +30,7 @@
     <!-- <link href="/template/style.css" rel="stylesheet" type="text/css" /> -->
 </head>
 <body>
+
 <!-- Navigation -->
 <nav class="navbar navbar-main navbar-static-top" role="navigation">
   <div class="container">
@@ -275,7 +276,11 @@
 <footer class="footer">
   <div class="container text-center">
     <div class="row">
-      <a href="https://www.facebook.com/PortetGym/" aria-label="Page Facebook PortetGym"><span class="fab fa-facebook social"></span></a>
+      <a href="https://www.facebook.com/PortetGym/" aria-label="Page Facebook de PortetGym"><span class="fab fa-facebook social"></span></a>
+      <a href="https://www.instagram.com/PortetGym/" aria-label="Instagram de PortetGym"><span class="fab fa-instagram social"></span></a>
+      <a href="https://www.youtube.com/channel/UCTDGQYG4ZNDcGUG8TnRzQyg" aria-label="Youtube de PortetGym"><span class="fab fa-youtube social"></span></a>
+    </div>
+    <div class="row">
     </div>
   </div>
   <div class="text-center bottom-row">
