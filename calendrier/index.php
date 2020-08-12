d4bbe4f80c6c3b3481762762b9744880000e6ba6
dvz31 <34982728+dvz31@users.noreply.github.com>
2020-08-12 10:51:07 +0200

    update calendrier/index.php - iframe
    modification de l'iframe pour ciblé le calendrier du compte portetgym@gmail.com

--- calendrier/index.php
+++ calendrier/index.php
@@ -25,7 +25,10 @@
     <?php include('../common/header.php'); ?>
 
     <div style="text-align: center">
-    <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23039BE5&amp;ctz=Europe%2FParis&amp;src=cnRnZzY3amNscGdrN3MzcWNvYTdzNjlzNmNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D81B60&amp;showTitle=0&amp;showNav=1&amp;showDate=1&amp;showTz=0&amp;showTabs=0&amp;showPrint=0" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    <!--DV : ancien iframe
+        <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23039BE5&amp;ctz=Europe%2FParis&amp;src=cnRnZzY3amNscGdrN3MzcWNvYTdzNjlzNmNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D81B60&amp;showTitle=0&amp;showNav=1&amp;showDate=1&amp;showTz=0&amp;showTabs=0&amp;showPrint=0" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    -->
+    <iframe src="https://calendar.google.com/calendar/embed?src=portetgym%40gmail.com&ctz=Europe%2FParis" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>    
     </div>
 
     <!-- Footer -->
